8730e12135693db1ce17b4faf5a61f52465bdd6f
annatopalidi <topalidisanna@gmail.com>
2021-04-10 13:59:54 +0200

    feat: complete task 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  let res = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count++;
+    } else if (count === 1) {
+      res += arr[i];
+    } else {
+      res += count + arr[i];
+      count = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
