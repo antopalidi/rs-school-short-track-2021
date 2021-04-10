92accd6eff4ba0a98e51a7d91bd4c8617726c550
annatopalidi <topalidisanna@gmail.com>
2021-04-10 14:53:18 +0200

    feat: complete task 12-dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,21 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const dict = {};
+  const data = domains.map((item) => item.split('.').reverse());
+  data.forEach((arr) => {
+    let key = '';
+    arr.forEach((item) => {
+      key += `.${item}`;
+      if (key in dict) {
+        dict[key]++;
+      } else {
+        dict[key] = 1;
+      }
+    });
+  });
+  return dict;
 }
 
 module.exports = getDNSStats;
